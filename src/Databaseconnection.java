d4f30f39a5facb578ea06c206c0c2bacbd4b66e1
itayanna <icage97@gmail.com>
2019-05-23 10:49:32 -0500

    data connection done

---
+++ src/Databaseconnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Databaseconnection {
+    private static String url = "jdbc:postgresql://138.197.107.95:5432/group4";
+    private static String user = "student";
+    private static String password = "C0d3Cr3w";
+
+    public Connection connect() {
+        Connection connect = null;
+        try{
+
+            connect = DriverManager.getConnection(url, user, password);
+        }
+        catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return connect;
+    }
+}
